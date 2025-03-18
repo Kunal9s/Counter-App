2f15d0e0675faec01f944aa63517d58a5d7fc4d5
Kunal shah <kunal8x@gmail.com>
2025-03-19 00:28:41 +0530

    message

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
   return (
     <>
       <div>
+        <h1>Counter App</h1>
         <h2>Count: {count}</h2>
         <button onClick={()=>setCount(count+1)}>Increment</button>
         <button onClick={()=>setCount(count-1)}>Decrement</button>
